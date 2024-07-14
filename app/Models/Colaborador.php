ba007d486322e72cb3fbf95e60710393fdff783a
Karen Daniela Diaz <calidad8844@gmail.com>
2024-07-14 17:10:40 -0500

    first commit

---
+++ app/Models/Colaborador.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Colaborador extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    protected $guarded=[];
+
+    public function proyectos(){
+        return $this->belongsToMany(Proyecto::class);
+    }
+    public function pagos(){
+        return $this->hasMany(Pago::class);
+    }
+}
